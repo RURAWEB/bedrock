19313aadd5c2d18e791ed350673d1cc0bcbd8bcc
RURAWEB <web@ruraweb.fr>
2022-02-20 00:49:29 +0100

    fix: ajout d'une condition dans le js du thème

--- web/app/themes/awc/js/awc_script.js
+++ web/app/themes/awc/js/awc_script.js
@@ -1,16 +1,19 @@
 $(document).ready(function($) {
-  const slider = tns({
-    container: "#slider",
-    items: 6,
-    controls: false,
-    center: true,
-    nav: false,
-    arrowKeys: true,
-    mouseDrag: true,
-    autoplay: true,
-    autoplayButtonOutput: false,
-    speed: 400
-  });
 
+  // Tiny Slider
+  if ( $('#slider').length > 0 ) {
+    const slider = tns({
+      container: '#slider',
+      items: 6,
+      controls: false,
+      center: true,
+      nav: false,
+      arrowKeys: true,
+      mouseDrag: true,
+      autoplay: true,
+      autoplayButtonOutput: false,
+      speed: 400
+    });
+  }
 
 }); // ----------->  FIN (document).ready(function($)

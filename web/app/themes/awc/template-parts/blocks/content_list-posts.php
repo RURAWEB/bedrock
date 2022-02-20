a749c316b893248d26d23dad7e9c6f9de27a695d
RURAWEB <web@ruraweb.fr>
2022-02-20 10:41:03 +0100

    feat: ajout du query avec les posts du CPT 'motscles'

--- web/app/themes/awc/template-parts/blocks/content_list-posts.php
+++ web/app/themes/awc/template-parts/blocks/content_list-posts.php
@@ -2,11 +2,28 @@
   $titre  = get_field('titre-blockPosts');
 ?>
 
-<div class="w-50 text-center mx-auto p-3">
+<section id="qualite" class="w-50 text-center mx-auto p-3">
   <?php if( $titre ) : ?>
     <h2><?php echo $titre; ?></h2>
   <?php endif; ?>
-</div>
+</section>
 
-<div class="w-75 p-3">
+<?php
+  $args = array(
+    'post_type'    => 'motscles',
+    'showposts'    => 4,
+  );
+
+  $motscles = new WP_Query( $args );
+  if( $motscles->have_posts() ) :
+?>
+  <section id="mots-cles" class="w-100 d-flex flex-wrap align-items-center justify-content-between gap-4 mt-4">
+    <?php
+      while( $motscles->have_posts() ) : $motscles->the_post();
+        the_content();
+      endwhile; wp_reset_postdata();
+    ?>
+  </section>
+<?php endif; ?>
+
 </div>

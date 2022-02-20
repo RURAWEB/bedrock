30ce2e676322c5dcb4e53e195c3974da09044e4a
RURAWEB <web@ruraweb.fr>
2022-02-20 15:57:49 +0100

    fix: correction <section> en <div>

--- web/app/themes/awc/template-parts/blocks/content_list-posts.php
+++ web/app/themes/awc/template-parts/blocks/content_list-posts.php
@@ -2,28 +2,28 @@
   $titre  = get_field('titre-blockPosts');
 ?>
 
-<section id="qualite" class="w-50 text-center mx-auto p-3">
+<div id="qualite" class="w-50 text-center mx-auto p-3">
   <?php if( $titre ) : ?>
     <h2><?php echo $titre; ?></h2>
   <?php endif; ?>
-</section>
+</div>
 
 <?php
   $args = array(
-    'post_type'    => 'motscles',
-    'showposts'    => 4,
+    'post_type' => 'motscles',
+    'showposts' => 4,
   );
 
   $motscles = new WP_Query( $args );
   if( $motscles->have_posts() ) :
 ?>
-  <section id="mots-cles" class="w-100 d-flex flex-wrap align-items-center justify-content-between gap-4 mt-4">
+  <div id="mots-cles" class="w-100 d-flex flex-wrap align-items-center justify-content-between gap-4 mt-4">
     <?php
       while( $motscles->have_posts() ) : $motscles->the_post();
         the_content();
       endwhile; wp_reset_postdata();
     ?>
-  </section>
+  </div>
 <?php endif; ?>
 
 </div>
